93a2e0e3bf6dca77c4185b5be92cdc708abf047c
HyunYoungGyu <stellarnetx@gmail.com>
2026-02-13 22:37:27 +0900

    feat: Add parallax effect and infinite scrolling background (#2)
    and Added cover scaling and camera-centered alignment for background rendering
    
    Co-authored-by: BaeJiwoo <76930864+BaeJiwoo@users.noreply.github.com>

---
+++ Assets/Script/Player/TESTPlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TESTPlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+}
